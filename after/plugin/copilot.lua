f9f1bdb3f916b1023bbb357b61f95cd4ad450c59
Olivier Gamache <olivier.gamache@radio-canada.ca>
2023-01-31 07:48:20 -0500

    batman

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,2 @@
+local node_path = '~/.nvm/versions/node/v17.9.0/bin/node';
+vim.g['copilot_node_command'] = node_path;
---
+++ lua/olgam4/themes/init.lua
@@ -0,0 +1,5 @@
+vim.opt.termguicolors = true
+vim.opt.guifont = 'Liga Hasklig:h15'
+vim.cmd.syntax 'enable'
+vim.cmd.syntax 'on'
+vim.cmd.colorscheme 'kanagawa'
